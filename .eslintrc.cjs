28c54d8a6bf52589349ceffd92800aa679bea977
iasakura <5370686+iasakura@users.noreply.github.com>
2026-02-06 10:27:44 +0900

    Fix ESLint config for react-refresh

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,13 +17,12 @@ module.exports = {
       version: "detect",
     },
   },
-  plugins: ["@typescript-eslint", "react", "react-hooks", "react-refresh"],
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
-    "plugin:react-refresh/recommended",
     "prettier",
   ],
   rules: {
